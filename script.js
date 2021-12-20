358d56b5105b19968287b6c581ea06d6c2356b84
BerdieVerhoosel <91963456+BerdieVerhoosel@users.noreply.github.com>
2021-12-20 15:19:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
-//Writing a JS function for the first time to make pizza!
 
-const makePizza = function () {
-    console.log('Ingredients: plain flower, water, olive oil, yeast, salt, sugar, tomatosauce, toppings of your choice.');
-    console.log('Step 1. Mix all the ingredients, except the tomatosauce and toppings');
-    console.log('Step 2. Kneed the ingredients till you have a ball of dough. Let rise for 1 hour.');
-    console.log('Step 3. Roll out and top with the tomatosauce and toppings.');
-    console.log('Step 4. Bake for 12 minutes at 240 degrees Celcius. Buon Apetito!');
+
+const paintWalls = function (wallType, paintColor) {
+    console.log("The " + wallType + " wall has been painted " + paintColor);
 };
 
-//makePizza();
-//makePizza();
-//makePizza();
+const color = ("green");
+const wall = ("South-east");
 
-const makeSushi = function () {
-    console.log('Ingredients: raw fresh fish, sushi rice, mirin, rice vinegar, wasabi');
-    console.log('Step 1. Make the sushi rice with mirin and rice vinegar. Let cool');
-    console.log('Step 2. Cut the fish in small slices');
-    console.log('Step 3. Make small riceballs and stick the fish on top. Press lightly.');
-    console.log('Serve with wasabi and soy sauce. Itadakimasu!');
-}
+paintWalls(wall, color);
 
-//makeSushi();
-
-makePizza();
-makePizza();
-makeSushi();
-makeSushi();
-makeSushi();
+//6. Yes the order of the arguments matter.
+//7. The arguments are logged in a different order.
+//8. Then it's allreight again.
